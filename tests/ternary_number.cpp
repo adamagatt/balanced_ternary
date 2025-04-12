4f57f6ef3293f583932a8c1902e4b8b9c325893c
Adam Gatt <adam.gatt@live.com.au>
2025-04-12 19:31:57 +0930

    Tests for inverse and shifting

--- tests/ternary_number.cpp
+++ tests/ternary_number.cpp
@@ -4,12 +4,49 @@
 #include "ternary_number.hpp"
 
 TEST(TernaryNumber, Output) {
-    TernaryNumber<8> num_50 {"+-0--"};
+    TernaryNumber<8> num_50 {"+-0--"}; // 50
     
     std::stringstream repr;
     repr << num_50;
 
     ASSERT_EQ(repr.str(), "000+-0-- (50)");
+}
+
+TEST (TernaryNumber, Inverse) {
+    TernaryNumber<8> num_35{"++0-"}; // 35
+    EXPECT_EQ(-num_35, TernaryNumber<8>{"--0+"}); // Negation is -35
+    ASSERT_EQ(-(-num_35), TernaryNumber<8>{"++0-"}); // Double negation is 35
+}
+
+TEST (TernaryNumber, LeftShift) {
+    TernaryNumber<8> num_neg_8{"-0+"}; // -8
+
+    EXPECT_EQ(num_neg_8 << 1, TernaryNumber<8>{"0000-0+0"});
+    EXPECT_EQ(num_neg_8 << 2, TernaryNumber<8>{"000-0+00"});
+    EXPECT_EQ(num_neg_8 << 3, TernaryNumber<8>{"00-0+000"});
+    EXPECT_EQ(num_neg_8 << 4, TernaryNumber<8>{"0-0+0000"});
+    EXPECT_EQ(num_neg_8 << 5, TernaryNumber<8>{"-0+00000"});
+    EXPECT_EQ(num_neg_8 << 6, TernaryNumber<8>{"0+000000"});
+    EXPECT_EQ(num_neg_8 << 7, TernaryNumber<8>{"+0000000"});
+    EXPECT_EQ(num_neg_8 << 8, TernaryNumber<8>{"00000000"});
+
+    auto shifting_num = num_neg_8;
+    shifting_num <<= 1;
+    EXPECT_EQ(shifting_num, TernaryNumber<8>{"0000-0+0"});
+    shifting_num <<= 1;
+    EXPECT_EQ(shifting_num, TernaryNumber<8>{"000-0+00"});
+    shifting_num <<= 1;
+    EXPECT_EQ(shifting_num, TernaryNumber<8>{"00-0+000"});
+    shifting_num <<= 1;
+    EXPECT_EQ(shifting_num, TernaryNumber<8>{"0-0+0000"});
+    shifting_num <<= 1;
+    EXPECT_EQ(shifting_num, TernaryNumber<8>{"-0+00000"});
+    shifting_num <<= 1;
+    EXPECT_EQ(shifting_num, TernaryNumber<8>{"0+000000"});
+    shifting_num <<= 1;
+    EXPECT_EQ(shifting_num, TernaryNumber<8>{"+0000000"});
+    shifting_num <<= 1;
+    ASSERT_EQ(shifting_num, TernaryNumber<8>{"00000000"});
 }
 
 TEST(TernaryNumber, BinaryOperations) {
